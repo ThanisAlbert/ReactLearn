364c9afa968d49064396cf410392bcdc72585c23
ThanisAlbert <thanisalbert@gmail.com>
2024-04-19 06:18:21 +0530

    Bootstrapadded_variable and method pass from parent to child

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
 
 export default function App() {
+
+  const[color, setColor]=useState("yellow")
+
+  const changeconstcolor = ()=>{   
+    setColor("red")
+  }
+
+  const changevariablecolor = (colorname)=>{   
+    setColor(colorname)
+  }
+
   return (
     <div>
-      LearnReact
+      <Header changevariablecolor={changevariablecolor}   changeconstcolor={changeconstcolor} colorname={color}></Header>     
     </div>
   );
+}
+
+ function Header(props){
+
+   return(
+
+    <div className="row">
+        Header is in {props.colorname} color
+        <button className="col-md-2" onClick={props.changeconstcolor}>Click constcolor</button>
+        <button className="col-md-2" onClick={()=>props.changevariablecolor("blue")}>Click variablecolor</button>
+        <div className="col-md-2"> col3</div> 
+        <div className="col-md-2"> col4</div> 
+    </div>
+
+   );
+
 }
 
